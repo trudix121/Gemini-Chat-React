7abb2068465ef73c875eb4391937bc1d63084d69
alexandrucarp14 <alexandrucarp14@gmail.com>
2025-01-26 11:53:16 +0000

    mini update

--- backend/src/ai.js
+++ backend/src/ai.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
-require('dotenv').config({path:'../.env.development'})
-const { GoogleGenerativeAI  } = require('@google/generative-ai')
-const ai = new GoogleGenerativeAI(process.env.API_KEY)
-const instructions = require('./model_instruction')
+const dotenv =require('dotenv')
+const config = dotenv.config({ path: '../.env.development' })
+
 
 router.use(express.urlencoded({extended:true}))
 router.use(express.json())
+let api_key;
+
+if (process.env.API_KEY) {
+    api_key = process.env.API_KEY
+}
+else {
+    require('dotenv').config({ path: '../.env' })
+    api_key = process.env.API_KEY
+}
+
+const { GoogleGenerativeAI  } = require('@google/generative-ai')
+const ai = new GoogleGenerativeAI(process.env.API_KEY)
+const instructions = require('./model_instruction')
 
 router.post('/', async (req,res)=>{
     const model = ai.getGenerativeModel({'model':`${req.body.model}`, 'systemInstruction':`${instructions[req.body.model_type]}`})
